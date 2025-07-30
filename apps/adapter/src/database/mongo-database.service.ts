a51112656ac111e79bda8669ab7191f0077486e2
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2025-07-30 19:10:59 -0300

    Fix: prisma client

--- apps/adapter/src/database/mongo-database.service.ts
+++ apps/adapter/src/database/mongo-database.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '.prisma/mongo-client';
+import { PrismaClient } from '../../../../node_modules/.prisma/mongo-client';
 
 @Injectable()
 export class MongoDatabaseService
--- apps/core-app/src/database/postgres-database.service.ts
+++ apps/core-app/src/database/postgres-database.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../../../node_modules/.prisma/postgres-client';
 
 @Injectable()
 export class PostgresDatabaseService
